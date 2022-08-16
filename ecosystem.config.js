bdc5e8887eb1e9cf0932b0701fb404c7ff036616
Sandhya5070 <110652362+Sandhya5070@users.noreply.github.com>
2022-08-16 16:44:25 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ user : 'sandhya',
 host : '52.66.85.95',
 ref : 'main',
 repo : 'git@github.com:Sandhya5070/nodejs.git',
-path : '/home/sandhya/node-app',
+path : '/home/sandhya/test',
 key : '/var/lib/jenkins/demo-node',	
 'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
 }
